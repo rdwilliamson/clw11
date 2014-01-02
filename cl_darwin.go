2daceb0cac698a790991e5b9a84fd3c4c19bdff0
Robert Williamson <williamson.robert@gmail.com>
2014-01-01 20:23:54 -0600

    Started to add apple specific stuff.

---
+++ cl_darwin.go
@@ -0,0 +1,22 @@
+package clw11
+
+/*
+#cgo LDFLAGS: -framework OpenCL
+
+#include "OpenCL/opencl.h"
+#include "OpenCL/cl_gl.h"
+*/
+import "C"
+import (
+	"errors"
+)
+
+const (
+	CGLShareGroupApple ContextProperties = C.CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE
+)
+
+var InvalidGLContextApple = errors.New("invalid GL context apple")
+
+func init() {
+	errorMap[C.CL_INVALID_GL_CONTEXT_APPLE] = InvalidGLContextApple
+}
--- context.go
+++ context.go
@@ -27,6 +27,18 @@ type (
 	ContextProperties C.cl_context_properties
 )
 
+const (
+	ContextPlatform ContextProperties = C.CL_CONTEXT_PLATFORM
+
+	// ContextD3D10Device ContextProperties = C.CL_CONTEXT_D3D10_DEVICE_KHR
+
+	// GLContext          ContextProperties = C.CL_GL_CONTEXT_KHR
+	// EGLDisplay         ContextProperties = C.CL_EGL_DISPLAY_KHR
+	// GLXDisplay         ContextProperties = C.CL_GLX_DISPLAY_KHR
+	// WGLHDC             ContextProperties = C.CL_WGL_HDC_KHR
+	// CGLSharegroup      ContextProperties = C.CL_CGL_SHAREGROUP_KHR
+)
+
 func CreateContext(properties []ContextProperties, devices []DeviceID) (Context, error) {
 
 	var propertiesValue *C.cl_context_properties
