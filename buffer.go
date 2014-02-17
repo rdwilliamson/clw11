774e6fa5722e92371bca2eb2c2058f89d38e8711
Robert Williamson <williamson.robert@gmail.com>
2014-02-16 21:01:32 -0600

    Looking at how to work with buffers.

--- buffer.go
+++ buffer.go
@@ -37,15 +37,10 @@ const (
 	MapWrite MapFlags = C.CL_MAP_WRITE
 )
 
-func CreateBuffer(context Context, flags MemoryFlags, size Size, host_ptr []byte) (Memory, error) {
-
-	var host unsafe.Pointer
-	if host_ptr != nil {
-		host = unsafe.Pointer(&host_ptr[0])
-	}
+func CreateBuffer(context Context, flags MemoryFlags, size Size, host_ptr unsafe.Pointer) (Memory, error) {
 
 	var err C.cl_int
-	memory := C.clCreateBuffer(context, C.cl_mem_flags(flags), C.size_t(size), host, &err)
+	memory := C.clCreateBuffer(context, C.cl_mem_flags(flags), C.size_t(size), host_ptr, &err)
 
 	return Memory(memory), toError(err)
 }
@@ -54,26 +49,29 @@ func ReleaseMemObject(memobject Memory) error {
 	return toError(C.clReleaseMemObject(memobject))
 }
 
-func EnqueueReadBuffer(command_queue CommandQueue, buffer Memory, blocking_read Bool, offset, cb Size, ptr []byte,
-	wait_list []Event, event *Event) error {
+func EnqueueReadBuffer(command_queue CommandQueue, buffer Memory, blocking_read Bool, offset, cb Size,
+	ptr unsafe.Pointer, wait_list []Event, event *Event) error {
 
 	event_wait_list, num_events_in_wait_list := toEventList(wait_list)
+
 	return toError(C.clEnqueueReadBuffer(command_queue, buffer, C.cl_bool(blocking_read), C.size_t(offset),
-		C.size_t(cb), unsafe.Pointer(&ptr[0]), num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
+		C.size_t(cb), ptr, num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
 }
 
-func EnqueueWriteBuffer(command_queue CommandQueue, buffer Memory, blocking_read Bool, offset, cb Size, ptr []byte,
-	wait_list []Event, event *Event) error {
+func EnqueueWriteBuffer(command_queue CommandQueue, buffer Memory, blocking_read Bool, offset, cb Size,
+	ptr unsafe.Pointer, wait_list []Event, event *Event) error {
 
 	event_wait_list, num_events_in_wait_list := toEventList(wait_list)
+
 	return toError(C.clEnqueueWriteBuffer(command_queue, buffer, C.cl_bool(blocking_read), C.size_t(offset),
-		C.size_t(cb), unsafe.Pointer(&ptr[0]), num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
+		C.size_t(cb), ptr, num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
 }
 
 func EnqueueCopyBuffer(command_queue CommandQueue, src_buffer, dst_buffer Memory, src_offset, dst_offset, cb Size,
 	wait_list []Event, event *Event) error {
 
 	event_wait_list, num_events_in_wait_list := toEventList(wait_list)
+
 	return toError(C.clEnqueueCopyBuffer(command_queue, src_buffer, dst_buffer, C.size_t(src_offset),
 		C.size_t(dst_offset), C.size_t(cb), num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
 }
@@ -82,9 +80,11 @@ func EnqueueMapBuffer(command_queue CommandQueue, buffer Memory, blocking_map Bo
 	wait_list []Event, event *Event) ([]byte, error) {
 
 	event_wait_list, num_events_in_wait_list := toEventList(wait_list)
+
 	var err C.cl_int
 	mapped := C.clEnqueueMapBuffer(command_queue, buffer, C.cl_bool(blocking_map), C.cl_map_flags(map_flags),
 		C.size_t(offset), C.size_t(cb), num_events_in_wait_list, event_wait_list, (*C.cl_event)(event), &err)
+
 	return toBytes(mapped, int(cb)), toError(err)
 }
 
@@ -92,6 +92,7 @@ func EnqueueUnmapMemObject(command_queue CommandQueue, memobj Memory, mapped_ptr
 	event *Event) error {
 
 	event_wait_list, num_events_in_wait_list := toEventList(wait_list)
+
 	return toError(C.clEnqueueUnmapMemObject(command_queue, memobj, unsafe.Pointer(&mapped_ptr[0]),
 		num_events_in_wait_list, event_wait_list, (*C.cl_event)(event)))
 }
