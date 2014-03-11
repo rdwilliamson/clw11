2ff215b3745ef0c80523e4a43ba9c800f1c39a8e
Robert Williamson <williamson.robert@gmail.com>
2014-03-10 23:02:06 -0500

    Added remaining command queue wrappers and documentation.

--- commandqueue.go
+++ commandqueue.go
@@ -23,16 +23,36 @@ const (
 	QueueProfilingEnable          CommandQueueProperties = C.CL_QUEUE_PROFILING_ENABLE
 )
 
+// Create a command-queue on a specific device.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateCommandQueue.html
 func CreateCommandQueue(context Context, device DeviceID, properties CommandQueueProperties) (CommandQueue, error) {
 	var err C.cl_int
 	result := C.clCreateCommandQueue(context, device, C.cl_command_queue_properties(properties), &err)
 	return CommandQueue(result), toError(err)
 }
 
+// Issues all previously queued OpenCL commands in a command-queue to the device
+// associated with the command-queue.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clFlush.html
 func Flush(cq CommandQueue) error {
 	return toError(C.clFlush(cq))
 }
 
+// Blocks until all previously queued OpenCL commands in a command-queue are
+// issued to the associated device and have completed.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clFinish.html
 func Finish(cq CommandQueue) error {
 	return toError(C.clFinish(cq))
+}
+
+// Increments the command_queue reference count.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clRetainCommandQueue.html
+func RetainCommandQueue(command_queue CommandQueue) error {
+	return toError(C.clRetainCommandQueue(command_queue))
+}
+
+// Decrements the command_queue reference count.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clReleaseCommandQueue.html
+func ReleaseCommandQueue(command_queue CommandQueue) error {
+	return toError(C.clReleaseCommandQueue(command_queue))
 }
