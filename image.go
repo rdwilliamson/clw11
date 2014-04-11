6c6961d14043866af83cbb63b61d9ee0bec3190b
Robert Williamson <williamson.robert@gmail.com>
2014-04-10 19:27:46 -0500

    Adding more image types.

--- image.go
+++ image.go
@@ -14,6 +14,7 @@ import "unsafe"
 type (
 	MemObjectType C.cl_mem_object_type
 	ImageFormat   C.cl_image_format
+	ImageInfo     C.cl_image_info
 	ChannelOrder  C.cl_channel_order
 	ChannelType   C.cl_channel_type
 )
@@ -58,6 +59,16 @@ const (
 	Float          ChannelType = C.CL_FLOAT
 )
 
+const (
+	ImageFormatInfo  ImageInfo = C.CL_IMAGE_FORMAT // Appended "Info" due to conflict with type.
+	ImageElementSize ImageInfo = C.CL_IMAGE_ELEMENT_SIZE
+	ImageRowPitch    ImageInfo = C.CL_IMAGE_ROW_PITCH
+	ImageSlicePitch  ImageInfo = C.CL_IMAGE_SLICE_PITCH
+	ImageWidth       ImageInfo = C.CL_IMAGE_WIDTH
+	ImageHeight      ImageInfo = C.CL_IMAGE_HEIGHT
+	ImageDepth       ImageInfo = C.CL_IMAGE_DEPTH
+)
+
 func CreateImageFormat(co ChannelOrder, ct ChannelType) ImageFormat {
 	return ImageFormat{C.cl_channel_order(co), C.cl_channel_type(ct)}
 }
@@ -70,6 +81,8 @@ func (f *ImageFormat) ChannelType() ChannelType {
 	return ChannelType(f.image_channel_data_type)
 }
 
+// Get the list of image formats supported by an OpenCL implementation.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetSupportedImageFormats.html
 func GetSupportedImageFormats(context Context, flags MemFlags, image_type MemObjectType, num_entries Uint,
 	image_formats *ImageFormat, num_image_formats *Uint) error {
 
@@ -77,6 +90,8 @@ func GetSupportedImageFormats(context Context, flags MemFlags, image_type MemObj
 		C.cl_uint(num_entries), (*C.cl_image_format)(image_formats), (*C.cl_uint)(num_image_formats)))
 }
 
+// Creates a 2D image object.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateImage2D.html
 func CreateImage2D(context Context, flags MemFlags, image_format ImageFormat, image_width, image_height,
 	image_row_pitch Size, host_ptr unsafe.Pointer) (Mem, error) {
 
@@ -85,4 +100,26 @@ func CreateImage2D(context Context, flags MemFlags, image_format ImageFormat, im
 		C.size_t(image_height), C.size_t(image_row_pitch), host_ptr, &err)
 
 	return Mem(mem), toError(err)
+}
+
+// Creates a 3D image object.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateImage3D.html
+func CreateImage3D(context Context, flags MemFlags, image_format ImageFormat, image_width, image_height, image_depth,
+	image_row_pitch, image_slice_pitch Size, host_ptr unsafe.Pointer) (Mem, error) {
+
+	var err C.cl_int
+	mem := C.clCreateImage3D(context, C.cl_mem_flags(flags), (*C.cl_image_format)(&image_format), C.size_t(image_width),
+		C.size_t(image_height), C.size_t(image_depth), C.size_t(image_row_pitch), C.size_t(image_slice_pitch), host_ptr,
+		&err)
+
+	return Mem(mem), toError(err)
+}
+
+// Get information specific to an image object.
+// http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetImageInfo.html
+func GetImageInfo(image Mem, param_name ImageInfo, param_value_size Size, param_value unsafe.Pointer,
+	param_value_size_ret *Size) error {
+
+	return toError(C.clGetImageInfo(image, C.cl_image_info(param_name), C.size_t(param_value_size), param_value,
+		(*C.size_t)(param_value_size_ret)))
 }
